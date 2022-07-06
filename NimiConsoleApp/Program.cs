283f3843e835b6ff19ba57d8be4fcbe2f6448808
Nimi77 <105428366+Nimi77@users.noreply.github.com>
2022-07-06 07:57:35 -0700

    Update Program.cs

--- NimiConsoleApp/Program.cs
+++ NimiConsoleApp/Program.cs
@@ -20,13 +20,43 @@ namespace NimiConsoleApp
 
             //Assignment 2
             string welcome = "Welcome to my first C# program";
-            double a = 5.0; 
-            bool b = true;
+            char letStart = 'S';
+            bool isCSharpEasy = true;
+            bool a = 111 >= 101;
+            int myFavouriteNumber = 31;
+            float b = 119/4;
+            float b1 = 100e3F;
+            long  b2 = 143508600000;
+            double c = 12e24;
+            char end = 'E';
+
             Console.WriteLine(welcome);
+            Console.WriteLine(letStart);
+          
+            Console.WriteLine(isCSharpEasy);
             Console.WriteLine(a);
+            Console.WriteLine(myFavouriteNumber);
             Console.WriteLine(b);
-            //Console.WriteLine("First C# program...");
-            //Console.WriteLine("Wella Backend Dev");
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(c);
+            Console.WriteLine(end);
+
+            //implicit and explicit
+            int myLevel = 4;
+            char myLevelChar = (char)myLevel;  //explicit
+
+            float myCpga = 4.98F;
+            double m = myCpga;
+
+            int courses =  11;
+            float n = courses;
+            long l = courses;
+
+            Console.WriteLine(myLevel);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+            Console.WriteLine(l);
 
 
             Console.ReadKey();
